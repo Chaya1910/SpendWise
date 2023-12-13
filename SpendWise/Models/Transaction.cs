6d00126e21b0eee74ce6f7f27b757a25efa9b7ea
Chaya <naiksnii74@gmail.com>
2023-12-14 00:46:22 +0530

    Add Transaction model

--- SpendWise/Models/Transaction.cs
+++ SpendWise/Models/Transaction.cs
@@ -1,6 +1,22 @@
-﻿namespace SpendWise.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpendWise.Models
 {
     public class Transaction
     {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string? Note { get; set; }
+
+        public int Amount { get; set;  }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
     }
 }
