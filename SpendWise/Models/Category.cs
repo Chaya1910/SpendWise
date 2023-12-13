f40955658add95048101b45eb65f4fd990af8e28
Chaya <naiksnii74@gmail.com>
2023-12-14 00:46:03 +0530

    Add Category model

--- SpendWise/Models/Category.cs
+++ SpendWise/Models/Category.cs
@@ -10,10 +10,12 @@ namespace SpendWise.Models
 
         [Column(TypeName = "nvarchar(50)")]
         public  string Title { get; set; }
-        [Column(TypeName = "nvarchar(5)")]
-        public string Icon { get; set; }
-        [Column(TypeName = "nvarchar(10)")]
 
-        public string Type { get; set; }
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+
     }
 }
